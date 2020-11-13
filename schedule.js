750ac22ef7eaa1ef29f17fe4011e47a81eabc2ca
eightycarry <calvin_lor@yahoo.com>
2020-11-12 18:14:13 -0600

    Class 8 Lab Question 3

--- schedule.js
+++ schedule.js
@@ -1,3 +1,13 @@
+var selectedDay;
 $(document).ready(function() {
 
+
+    selectedDay = $(radio).checked;
+    if (selectedDay === "monday") {
+        switch (selectedDay) {
+            case "monday":
+                $("#event").text("Work")
+                $("#time").text("11:00 PM - 7:30 AM")
+        }
+    }
 });
